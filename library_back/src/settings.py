d04bb4cddbd09484657a4a150069a608b6456f07
Matheus Fonseca Sousa <54778783+gatotabaco@users.noreply.github.com>
2022-09-12 20:03:09 -0300

    Banco mais back

--- library_back/src/settings.py
+++ library_back/src/settings.py
@@ -88,16 +88,16 @@ POSTGRES_DB = os.getenv('POSTGRES_DB')
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-POSTGRES_PORT = int(os.getenv('POSTGRES_PORT'))
+POSTGRES_PORT = int(os.getenv('POSTGRES_PORT', 5432))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': POSTGRES_DB,
-        'USER': POSTGRES_USER,
-        'PASSWORD': POSTGRES_PASSWORD,
-        'HOST': POSTGRES_HOST,
-        'PORT': POSTGRES_PORT,
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
